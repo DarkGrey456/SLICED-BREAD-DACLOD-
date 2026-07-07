f06bc77be7c92a6c81e8c04e92875b09132b9005
DarkGrey456 <dchn.256@gmail.com>
2026-07-07 10:01:39 +0100

    Add files via upload

--- DACLOD/compute_helper.gd
+++ DACLOD/compute_helper.gd
@@ -9,11 +9,10 @@ var compute: EasyCompute = EasyCompute.new()
 
 
 
-func texture_fill3()->PackedVector4Array:
+func generate_AABB_and_occluders(height_map:Image)->PackedVector4Array:
 	compute.load_shader("fill", "res://assets/compute/fill42.glsl")
-	
-	var tex :CompressedTexture2D= load("res://assets/Heightmaps/ISLAND_4k_2/height2.exr")
-	var image = tex.get_image()
+		
+	var image = height_map
 	if image.is_compressed():
 		image.decompress()	
 	image.convert(Image.FORMAT_RF)
@@ -41,7 +40,9 @@ func texture_fill3()->PackedVector4Array:
 	
 	var final_image_data = compute.fetch_texture("data_output_image2")
 	var float_data = final_image_data.to_vector4_array()
-
+	
+	compute.unload_shader("fill_min_max")
+	
 	return float_data
 	
 	#var box_array = []
@@ -72,3 +73,95 @@ func texture_fill3()->PackedVector4Array:
 	#
 	#var output_image = Image.create_from_data(64, 64, false, Image.FORMAT_RGBAF, final_image_data)
 	#var texture2 = ImageTexture.create_from_image(output_image)
+
+
+#=======================================================================================================
+
+#layout(set = 0, binding = 0) uniform sampler2D height_tex;
+#layout(set = 0, binding = 1) uniform sampler2D splat_tex;
+#
+#layout(set = 0, binding = 2) uniform sampler2D tex1;
+#layout(set = 0, binding = 3) uniform sampler2D tex3;
+#
+#
+#layout(std140,set = 1, binding = 4) readonly buffer Faces{
+#
+	#vec3 data[];
+#} faces;
+#
+#
+#layout(set = 0, binding = 5, std430) restrict readonly buffer Params {
+	#vec2 grid_coord;
+	#vec4 uv_scale;
+	#uint splat_col;
+	#float HEIGHT_SCALE;
+	#
+#}params;
+#
+#
+#layout(std140,set = 1, binding = 6) writeonly buffer OutputFaces{
+#=======================================================================================================
+func generate_physics_shape(height_map:Image, 
+							splat_map:Image, 
+							tex1:Image, 
+							tex2:Image, 
+							grid_coord:Vector2,
+							uv_scale:Vector4,
+							splat_col:int,
+							HEIGHT_SCALE:float,
+							mesh_verts:Image):
+								
+	compute.load_shader("compute_height", "res://assets/compute/compute_physics_model.glsl")
+		
+	var image = height_map
+	if image.is_compressed():
+		image.decompress()	
+	image.convert(Image.FORMAT_RF)
+	var height_texture = ImageTexture.create_from_image(image)
+	var source_size = 4096
+	
+	var splat_image = splat_map
+	if splat_image.is_compressed():
+		splat_image.decompress()	
+	splat_image.convert(Image.FORMAT_RGBA8)
+	
+	var tex1_image = tex1
+	if tex1_image.is_compressed():
+		tex1_image.decompress()	
+	tex1_image.convert(Image.FORMAT_RGBA8)
+	
+	var tex2_image = tex2
+	if tex2_image.is_compressed():
+		tex2_image.decompress()	
+	tex2_image.convert(Image.FORMAT_RGBA8)	
+
+
+	compute.register_sampler("height_tex", 0, source_size, source_size, image.get_data(), RenderingDevice.DATA_FORMAT_R32_SFLOAT)	
+	compute.register_sampler("splat_tex", 5, source_size, source_size, splat_image.get_data(), RenderingDevice.DATA_FORMAT_R8G8B8A8_UNORM)	
+	compute.register_sampler("tex1", 6, 512, 512, tex1_image.get_data(), RenderingDevice.DATA_FORMAT_R8G8B8A8_UNORM)	
+	compute.register_sampler("tex3",7, 512, 512, tex2_image.get_data(), RenderingDevice.DATA_FORMAT_R8G8B8A8_UNORM)	
+		
+	if mesh_verts.is_compressed():
+		mesh_verts.decompress()
+	compute.register_texture("mesh_verts", 8, 128, 128, mesh_verts.get_data(), RenderingDevice.DATA_FORMAT_R32G32B32A32_SFLOAT)
+	
+	
+	
+
+
+	compute.register_texture("output_verts", 9, 128, 128,[], RenderingDevice.DATA_FORMAT_R32G32B32A32_SFLOAT)
+	#var params = PackedFloat32Array([grid_coord.x, grid_coord.y, 
+									#uv_scale.x, uv_scale.y, uv_scale.z, uv_scale.w, 
+									#float(splat_col), 
+									#HEIGHT_SCALE])
+	#var params_byte_array = params.to_byte_array()
+	#compute.register_storage_buffer("Params", 6, params_byte_array.size(), params_byte_array)
+	# FIRST PASS, Find min and max height values
+
+	compute.execute("compute_height", 8, 8)
+	compute.sync()
+	
+	var final_mesh_data = compute.fetch_buffer("output_verts")
+	var float_data = final_mesh_data.to_vector3_array()
+	print(float_data)
+	compute.unload_shader("compute_height")
--- DACLOD/hmap_loader.gd
+++ DACLOD/hmap_loader.gd
@@ -8,6 +8,7 @@ extends Node3D
 
 
 #@onready var collision_shape_3d: CollisionShape3D = $"../StaticBody3D/CollisionShape3D"
+@export var height_map_name :String = "res://assets/Heightmaps/ISLAND_4k_2/height2.exr"
 @export var height_map_texture:Texture2D
 var hmap:Image 
 
@@ -144,6 +145,7 @@ func flatten_grid_pattern_at_point(p:Vector2i, tracked_grid_nodes: Array, b_thre
 												#float(py)*cell_size + float(l)*cell_size/2.0  )
 						rect.size = Vector2(cell_size, cell_size)	
 						#rect.size = Vector2(cell_size/2.0, cell_size/2.0)	
+						
 						WorkerThreadPool.add_task(func():thread_create_colliders(rect))
 					else:
 						#else don't thread
@@ -164,13 +166,15 @@ func flatten_grid_pattern_at_point(p:Vector2i, tracked_grid_nodes: Array, b_thre
 #=======================================================================================
 
 func thread_create_colliders(rect:Rect2):
-	thread_generate(rect, false)		
+	thread_generate(rect)		
 				
-func thread_create_occluders(rect:Rect2):
-	thread_generate(rect, true)
+
 	
 	
 var data_mesh_grid:Dictionary
+var mesh_faces :PackedVector3Array
+var mesh_verts:PackedVector3Array
+
 func create_occluders()->void:
 	#if not Engine.is_editor_hint() and first_time:
 	first_time = false
@@ -179,12 +183,31 @@ func create_occluders()->void:
 		#create_collision_shape()
 		
 		# but we scan the heightmap twice at the moment
-	data_mesh_grid = get_mesh_vertices_and_indices(plane__128.mesh)
+	#data_mesh_grid = get_mesh_vertices_and_indices(plane__128.mesh)
+	mesh_faces = get_mesh_faces(plane__128.mesh)["faces"]
+	mesh_verts = get_mesh_vertices(plane__128.mesh)["vertices"]
+	print(sqrt(mesh_verts.size()))
+	#var vertex_image = Image.new()
+	#var n:float=sqrt(mesh_verts.size())
+	#vertex_image.resize(n,n)
+	#vertex_image.convert(Image.FORMAT_RGBAF)
+	#for i in mesh_verts.size():
+		#var v:Vector3 = mesh_verts[i]
+		#vertex_image.set_pixel(int(v.x), int(v.z), Color(v.x, v.y, v.z, 1.0))
 		
 	# Launch the compute shader kernal to quickly compute the AABB's 	
-	var computed_heights :PackedVector4Array= compute_helper.texture_fill3()
+	var computed_heights :PackedVector4Array= compute_helper.generate_AABB_and_occluders(hmap)
 	
-	
+	#var grid_coord = world_coords_to_grid_coords(player.global_position.x, player.global_position.z)
+	#compute_helper.generate_physics_shape( hmap, 
+							#self.splat_map, 
+							#self.alb1, 
+							#self.alb3, 
+							#Vector2(float(grid_coord.x),float(grid_coord.y)),
+							#UV_SCALE,
+							#float(channel_for_splat),
+							#HEIGHT_SCALE,
+							#vertex_image)
 	for J in range(63):
 		if J % 2 == 0:
 			for I in range(63):
@@ -213,9 +236,9 @@ func create_occluders()->void:
 					
 					var local_height = max(local_max_h - local_min_h, 1.0)
 					
-					grid[ g_x ][ g_y ].storage[0].aabb = AABB( Vector3(	0.0, 
+					grid[ g_x ][ g_y ].storage[0].aabb = AABB( Vector3(	0.5, 
 																	   	local_min_h, 
-																		0.0),
+																		0.5),
 															   Vector3( cell_size,
 																		local_height, 
 																		cell_size) )
@@ -243,9 +266,9 @@ func create_occluders()->void:
 						 Callable(self, 
 						 		  "_on_tree_entered_computed_occ")
 								  .bind( occl,
-										 Vector3(rect.position.x+cell_size/2.0, 
+										 Vector3(rect.position.x+cell_size/2.0 + 0.5, 
 												 min_h/2.0, 
-												 rect.position.y+cell_size/2.0)	))	
+												 rect.position.y+cell_size/2.0 + + 0.5)))	
 
 													
 			for ch in grid[i][j].storage[0].node.get_children():
@@ -267,7 +290,60 @@ func create_box_mesh(pos:Vector3, size:Vector3)->MeshInstance3D:
 	occluder_mesh.global_position = pos
 	return occluder_mesh
 		
-		
+func get_mesh_faces(mesh:Mesh)->Dictionary:
+	var result := {
+		"faces": []
+	}	
+	var mdt := MeshDataTool.new()
+	
+	for surface in mesh.get_surface_count():
+		var err := mdt.create_from_surface(mesh, surface)
+		if err != OK:
+			print("Failed to read surface %d" % surface)
+			continue
+
+		# Extract indices (faces are always triangles)
+		var face_count := mdt.get_face_count()
+		for f in face_count:
+			var i1 := mdt.get_face_vertex(f, 0)
+			var i2 := mdt.get_face_vertex(f, 1)
+			var i3 := mdt.get_face_vertex(f, 2)
+
+			result["faces"].append(mdt.get_vertex(i1))
+			result["faces"].append(mdt.get_vertex(i2))
+			result["faces"].append(mdt.get_vertex(i3))
+
+		mdt.clear()	
+	
+	return result
+	
+	
+	# Reads all vertices and indices from every surface of a Mesh
+func get_mesh_vertices(mesh: Mesh) -> Dictionary:
+	var result := {
+		"vertices": [],
+	}
+	if mesh == null:
+		push_error("Mesh is null.")
+		return result
+
+	var mdt := MeshDataTool.new()
+
+	# Loop through all surfaces because a Mesh can have more than one
+	for surface in mesh.get_surface_count():
+		var err := mdt.create_from_surface(mesh, surface)
+		if err != OK:
+			print("Failed to read surface %d" % surface)
+			continue
+
+		# Extract vertices
+		var vtx_count := mdt.get_vertex_count()
+		for i in vtx_count:
+			var vert := mdt.get_vertex(i)
+			result["vertices"].append(vert)
+			
+	return result	
+	
 	# Reads all vertices and indices from every surface of a Mesh
 func get_mesh_vertices_and_indices(mesh: Mesh) -> Dictionary:
 	var result := {
@@ -310,7 +386,7 @@ func get_mesh_vertices_and_indices(mesh: Mesh) -> Dictionary:
 	return result
 
 
-@onready var plane__128: MeshInstance3D = $tiles_256/Plane__256
+@onready var plane__128: MeshInstance3D = $tiles_256/Plane__128
 
 @export var player:CharacterBody3D
 
@@ -324,11 +400,19 @@ func get_mesh_vertices_and_indices(mesh: Mesh) -> Dictionary:
 # heightmap accessor functions and Images
 #=======================================================================================
 func load_image()->void:
-	hmap = LoadLargeHeightMap("res://assets/Heightmaps/ISLAND_4k_2/height2.exr")
+	hmap = LoadLargeHeightMap()
 	#self.HEIGHT_SCALE/xmax
-	create_normal_map(hmap, self.HEIGHT_SCALE, "res://assets/Heightmaps/ISLAND_4k_2/normal_map.png")
+	#var path = height_map_name
+	height_map_name = height_map_texture.resource_path
+		
+	var last_slash = height_map_name.rfind("/")
+	var substr = height_map_name.substr(0,last_slash)
+	var normal_map_path = substr + "/normal_map.png"	
+	
+	create_normal_map(hmap, self.HEIGHT_SCALE, normal_map_path)
 	if hmap.is_compressed():
 		hmap.decompress()	
+	#await gpuSplat.changed
 	splat_map = gpuSplat.get_image()
 	if splat_map.is_compressed():
 		splat_map.decompress()
@@ -342,6 +426,7 @@ func load_shader_images()->void:
 	alb2 = alb_h_2.get_image()
 	alb3 = alb_h_3.get_image()
 	alb4 = alb_h_4.get_image()
+	
 	
 	if alb1.is_compressed():
 		alb1.decompress()
@@ -361,7 +446,7 @@ func load_shader_images()->void:
 
 	
 
-func LoadLargeHeightMap(filename:String)->Image:
+func LoadLargeHeightMap()->Image:
 	#var height:Texture2D = load(filename)
 
 	return height_map_texture.get_image()
@@ -390,17 +475,14 @@ func create_normal_map(height_map:Image, bump_scale:float, path:String)->void:
 	
 	if (gpuNormals == null) :#or gpuNormals.is_empty():
 		nmap = height_map.duplicate(true)
-		var path_to_hmap = height_map_texture.resource_path
-		
-		var last_slash = path_to_hmap.rfind("/")
-		var substr = path_to_hmap.substr(0,last_slash)
+
 		
 		nmap.bump_map_to_normal_map(bump_scale)
 		gpuNormals = ImageTexture.new()
 		gpuNormals.create_from_image(nmap)
 	
 		# Save with error checking
-		var save_err := nmap.save_png(substr + "/normal_map.png")
+		var save_err := nmap.save_png(path)
 		if save_err != OK:
 			push_error("Failed to save normal map: %s" % path)
 			return
@@ -416,11 +498,48 @@ func create_normal_map(height_map:Image, bump_scale:float, path:String)->void:
 		nmap = gpuNormals.get_image()
 		
 
+func sample_image_bilnear_bump(image:Image,x:float, y:float, scale:float, divisor:float, size:float)->float:
+	
+	var u_1 = scale * x / divisor  
+	var v_1 = scale * y / divisor 
+		
+	var u__1 =  (u_1 - int(u_1))
+	var v__1 =    (v_1 - int(v_1))
+	
+	var u1 = size * u__1
+	var v1 = size * v__1
+		
+	var u12 = u1 + 1 
+	if u12 > size-1: u12 = 0
+	var v12 = v1 + 1
+	if v12 > size-1: v12 = 0
+
+	# bilinear filter attempt 
+	var m_uv1 := Vector2i( u1,v1)
+	var m_uv12 := Vector2i( u12,v1)
+	var m_uv13 := Vector2i( u1,v12)
+	var m_uv14 := Vector2i( u12,v12)
+	var disp11 = image.get_pixelv( m_uv1 ).a	
+	var disp12 = image.get_pixelv( m_uv12 ).a
+	var disp13 = image.get_pixelv( m_uv13 ).a
+	var disp14 = image.get_pixelv( m_uv14 ).a
+	var disp1 = lerp ( lerp(disp11,disp12,u__1), lerp(disp13,disp14,u__1), v__1)	
+
+	return disp1
+
 @export var channel_for_splat:int
 
 func get_map_values( x:float, z:float, X:int, Z:int):
 	var normal_pix:Color = nmap.get_pixel(X+int(x), Z+int(z))
-	var normal = Vector3(normal_pix.r, normal_pix.g, normal_pix.b ).normalized()
+	#var normal = Vector3(normal_pix.r, normal_pix.g, normal_pix.b ).normalized()
+	
+	var H1 = HEIGHT_SCALE*hmap.get_pixel(X + int(x-1), Z + int(z)).r
+	var H2 = HEIGHT_SCALE*hmap.get_pixel(X + int(x+1), Z + int(z)).r
+	var H3 = HEIGHT_SCALE*hmap.get_pixel(X + int(x), Z + int(z-1)).r
+	var H4 = HEIGHT_SCALE*hmap.get_pixel(X + int(x), Z + int(z+1)).r
+	
+	var normal = -Vector3(2.0*(H2 - H1), -4.0, 2.0*(H4-H3) ).normalized();
+	
 	var splat:Color = splat_map.get_pixel( X+int(x), Z+int(z) )
 	#x += X
 	#z += Z
@@ -428,21 +547,17 @@ func get_map_values( x:float, z:float, X:int, Z:int):
 	# TEXTURE_DIVISOR is 127 here, 128-1, because x never reaches 128 for each tile, it goes 0-127
 	# BUT ... viewing with the collision mesh showed the results were bettwe when the shader
 	# had 128 as the divisor ... why ? I don't know, its still broken.
-	 
-	var u1 = 	int(512.0*(x * UV_SCALE.x/ TEXTURE_DIVISOR  - floor(x * UV_SCALE.x/ TEXTURE_DIVISOR  ) ))
-	var v1 = 	int(512.0*(z * UV_SCALE.x/ TEXTURE_DIVISOR  - floor(z * UV_SCALE.x/ TEXTURE_DIVISOR  ) ))
-	var m_uv1 := Vector2i( u1,v1 )
-	var disp1 = alb1.get_pixelv( m_uv1 ).a	
+	
+	var disp1 =sample_image_bilnear_bump(alb1,x,z,UV_SCALE.x,128.0,512)
 
 	var u2 = 	int(512.0*(x * UV_SCALE.x/ TEXTURE_DIVISOR  - floor(x * UV_SCALE.x/ TEXTURE_DIVISOR  ) ))
 	var v2 = 	int(512.0*(z * UV_SCALE.x/ TEXTURE_DIVISOR  - floor(z * UV_SCALE.x/ TEXTURE_DIVISOR  ) ))
 	var m_uv2:= Vector2i( u2,v2 )
 	var disp2 = alb2.get_pixelv( m_uv2 ).a	
 	
-	var u3 = 	int(512.0*(x * UV_SCALE.z/ TEXTURE_DIVISOR  - floor(x * UV_SCALE.z/ TEXTURE_DIVISOR  ) ))
-	var v3 = 	int(512.0*(z * UV_SCALE.z/ TEXTURE_DIVISOR  - floor(z * UV_SCALE.z/ TEXTURE_DIVISOR  ) ))
-	var m_uv3 := Vector2i( u3,v3 )
-	var disp3 = alb3.get_pixelv( m_uv3 ).a	
+	var disp3 =sample_image_bilnear_bump(alb3,x,z,UV_SCALE.z,128.0,512)
+
+
 
 	var u4 = 	int( 512.0 * fposmod( (x * UV_SCALE.w)/ TEXTURE_DIVISOR, 1.0  )) 
 	var v4 = 	int( 512.0 * fposmod( (z * UV_SCALE.w)/ TEXTURE_DIVISOR, 1.0  )) 
@@ -470,101 +585,52 @@ func get_splat_color(splat)->float:
 #=======================================================================================
 # Thread function
 #=======================================================================================	
-func thread_generate(rect: Rect2, gen_occluders:bool)->void:
+func thread_generate(rect: Rect2)->void:
 	
-	if not gen_occluders:
+	
 
-		var mesh_data = generate_mesh_data(rect)
-		var array_mesh = ArrayMesh.new()
-		array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, mesh_data)
+	var mesh_data = generate_mesh_data(rect)
+
+	var static_body:StaticBody3D = StaticBody3D.new()
+	var collision_shape := CollisionShape3D.new()
 		
-		var static_body:StaticBody3D = StaticBody3D.new()
-		var collision_shape := CollisionShape3D.new()
-		
-		var polygon_shape := ConcavePolygonShape3D.new()
-		polygon_shape.set_faces(array_mesh.get_faces())
-		
-		collision_shape.shape = polygon_shape
-		static_body.add_child(collision_shape)
-		
-		add_child.call_deferred(static_body)
-		
-		
-		
-		static_body.connect("tree_entered", Callable(self, 
-				"_on_tree_entered").bind( static_body,
-										Vector3(rect.position.x + 0.5, 
-												0.0, 
-												rect.position.y + 0.5)))
+	var polygon_shape := ConcavePolygonShape3D.new()
+	polygon_shape.set_faces(mesh_data)
+
+	#create_physics_shape.call_deferred(mesh_data,
+									   #Vector3( rect.position.x , 
+												#0.0, 
+												#rect.position.y ))
+	collision_shape.shape = polygon_shape
+	static_body.add_child(collision_shape)
+		#
+	add_child.call_deferred(static_body)
+		#
+		#
+		#
+	static_body.connect("tree_entered", Callable(self, 
+			"_on_tree_entered").bind( static_body,
+									Vector3(rect.position.x  , 
+											0.0, 
+											rect.position.y )))
 		
 		
 #=======================================================================================
 # Colliosion Mesh
 #=======================================================================================	
-# regression to Quadtree terain mesh helper functions
+
 func generate_mesh_data(rect: Rect2) -> Array:
 
-	var SIZE = (resolution) + 1
-	var step_size = rect.size.x / resolution
+	var mesh_faces_local :PackedVector3Array=  PackedVector3Array()
 	
-	# Step 1: Generate Base Height Data
-	var height_data = PackedFloat32Array()
-	height_data.resize(SIZE * SIZE)
+	for v in mesh_faces:
+		v.y = get_altitude(Vector3(rect.position.x+v.x, 0, rect.position.y+v.z))
+		var disp_dict = get_map_values(v.x, v.z, rect.position.x, rect.position.y)#x/2.0,z/2.0
+		v += 5.0 * disp_dict["normal"] * disp_dict["height"]# - 2.5 * disp_dict["normal"]
+		mesh_faces_local.append(v)
+		
+	return mesh_faces_local	
 
-	min_h = 100000	
-	max_h = -100000	
-
-	var vertices = PackedVector3Array()
-	var uvs = PackedVector2Array()
-	var indices = PackedInt32Array()
-	
-	vertices.resize(SIZE * SIZE)
-	uvs.resize(SIZE * SIZE)
-
-	for z in SIZE:
-		for x in SIZE:
-			var pos_x = rect.position.x + (x )  #(x/2.0 
-			var pos_z = rect.position.y + (z )
-			var h:float = get_altitude(Vector3(pos_x, 0, pos_z))
-			if h < min_h:
-				min_h = h
-			if h > max_h:
-				max_h = h
-			var i = x + z * SIZE
-	
-			vertices[i] = Vector3(x, h, z)  #Vector3(x/2.0, h, z/2.0)
-			uvs[i] = Vector2( (rect.position.x+float(x)) / (resolution), (rect.position.y+float(z)) / (resolution))
-			var disp_dict = get_map_values(x,z,rect.position.x, rect.position.y)#x/2.0,z/2.0
-			vertices[i] += 5.0 * disp_dict["normal"] * disp_dict["height"] - 2.5 * disp_dict["normal"]
-
-
-	# Indices logic
-	for z in SIZE-1:
-		for x in SIZE-1:
-			var i = x + z * SIZE
-			indices.append(i)
-			indices.append(i + SIZE + 1) 
-			indices.append(i + SIZE)
-			indices.append(i + 1)
-			indices.append(i )
-			indices.append(i + SIZE+1)
-			
-			#indices.append(i)
-			#indices.append(i + 1) 
-			#indices.append(i + SIZE)
-			#indices.append(i + 1)
-			#indices.append(i + SIZE + 1)
-			#indices.append(i + SIZE)			
-	
-	var arrays = []
-	arrays.resize(Mesh.ARRAY_MAX)
-	arrays[Mesh.ARRAY_VERTEX] = vertices
-	arrays[Mesh.ARRAY_TEX_UV] = uvs
-	arrays[Mesh.ARRAY_INDEX] = indices
-	
-	return arrays	
-
-	
 
 	
 func _on_tree_entered(node:Node3D,pos:Vector3):
@@ -574,13 +640,25 @@ func _on_tree_entered(node:Node3D,pos:Vector3):
 	#print( "added node " + node.name +" to tree at " + var_to_str(pos) )
 	
 #=======================================================================================
-# get AABB and Occluder
+# Physics Shape (NOT WORKING)
 #=======================================================================================
-
+func create_physics_shape( data:PackedVector3Array, pos:Vector3) ->void:
+	# 1. Create a box shape
 	
-
-	
-
+	var shape_rid = PhysicsServer3D.concave_polygon_shape_create()
+	var mesh_data = { "faces": data, "back_face_collision": false}
+	PhysicsServer3D.shape_set_data(shape_rid, mesh_data) 
+	# 2. Create a rigid body
+	var body_rid = PhysicsServer3D.body_create()
+	PhysicsServer3D.body_set_mode(body_rid, PhysicsServer3D.BODY_MODE_STATIC)
+	# 3. Assign the shape to the body
+	PhysicsServer3D.body_add_shape(body_rid, shape_rid)
+	# 4. Set the body’s transform (position in world)
+	var transform = Transform3D(Basis(), pos)
+	PhysicsServer3D.body_set_state(body_rid, PhysicsServer3D.BODY_STATE_TRANSFORM, transform)
+	# 5. Add the body to the space
+	var space = get_world_3d().space
+	PhysicsServer3D.body_set_space(body_rid, space)
 	
 		
 
@@ -647,10 +725,10 @@ func _ready() -> void:
 	#if not Engine.is_editor_hint():
 	load_image()
 		
-	create_occluders()
+
 							
 	setup_shader()			
-	
+	create_occluders()
 	var grid_coords = world_coords_to_grid_coords(player.global_position.x, player.global_position.z)
 	self.flatten_grid_pattern_at_point(grid_coords, tracked_grid_nodes,false)
 
